88ae214e1e0f0c10e6d3f8dc21910657f219b462
Anton Evdokimov <free0u@gmail.com>
2012-10-10 23:43:16 +0400

    Add Eclipse project

---
+++ src/ru/ifmo/rain/evdokimov/MainActivity.java
@@ -0,0 +1,30 @@
+package ru.ifmo.rain.evdokimov;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+	class MainView extends View {
+		public MainView(Context context) {
+			super(context);
+		}
+		
+		@Override
+		public void onDraw(Canvas canvas) {
+			Paint paint = new Paint();
+			paint.setColor(Color.RED);
+			canvas.drawRect(10, 10, 110, 110, paint);
+		}
+	}
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MainView(this));
+    }
+}
