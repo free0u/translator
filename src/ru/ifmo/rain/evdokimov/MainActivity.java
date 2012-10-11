f4350b2028131be96fc0ff552fa5cac8481b2284
Anton Evdokimov <free0u@gmail.com>
2012-10-11 22:35:00 +0400

    remove title of main window

--- src/ru/ifmo/rain/evdokimov/MainActivity.java
+++ src/ru/ifmo/rain/evdokimov/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends Activity {
 	class MainView extends View {
@@ -25,6 +26,8 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(new MainView(this));
+        
     }
 }
