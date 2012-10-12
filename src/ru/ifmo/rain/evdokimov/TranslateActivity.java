29d09f37af51aa07e69e80041e87c3262bb8e097
Anton Evdokimov <free0u@gmail.com>
2012-10-12 17:27:52 +0400

    remove title of translate window

--- src/ru/ifmo/rain/evdokimov/TranslateActivity.java
+++ src/ru/ifmo/rain/evdokimov/TranslateActivity.java
@@ -2,6 +2,7 @@ package ru.ifmo.rain.evdokimov;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class TranslateActivity extends Activity {
 	
@@ -9,6 +10,7 @@ public class TranslateActivity extends Activity {
 	
 	@Override
 	public void onCreate(Bundle state) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(state);
 		setContentView(R.layout.translate);
 	}
